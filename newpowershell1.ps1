f460d4133affc7ac47bed02ed75b009c7754c4b3
graj0815 <mailfromgowthamraj@gmail.com>
2025-11-06 02:25:51 +0530

    version 2 with colors

--- newpowershell1.ps1
+++ newpowershell1.ps1
@@ -1,2 +1,3 @@
-Write-Output "this the fresh powershell learning test1"
-Write-Output "this is the second line of the fresh powershell learning test1"
+Write-Output "this the fresh powershell learning test1" -foregroundcolor Yellow
+Write-Output "this is the second line of the fresh powershell learning test1" -foregroundcolor Green
+Write-Output "this is the third line of the fresh powershell learning test1" -foregroundcolor Cyan
